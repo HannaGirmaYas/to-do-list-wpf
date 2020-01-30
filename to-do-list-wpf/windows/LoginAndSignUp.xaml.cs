99afc228012a65e52a133e575a3fbb953310a4ab
HannaGirmaYas <hannagirma16@gmail.com>
2020-01-30 20:49:53 +0300

    Login and SignUp + Settings Front end Full

---
+++ to-do-list-wpf/windows/LoginAndSignUp.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace to_do_list_wpf.windows
+{
+    /// <summary>
+    /// Interaction logic for LoginAndSignUp.xaml
+    /// </summary>
+    public partial class LoginAndSignUp : Window
+    {
+        public LoginAndSignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}

4678563b286d2f6f2f71fc4b6f6a5ca944203002
HannaGirmaYas <hannagirma16@gmail.com>
2020-01-31 16:06:31 +0300

    Added Files ...again

---
+++ to-do-list-wpf/Model/FireBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+
+namespace to_do_list_wpf.Model
+{
+    
+
+    namespace to_do_list_wpf.Models
+    {
+        class FBToDo
+        {
+            IFirebaseClient client;
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = "0ZnJY5HInBGNHaMSJCgr0PvGmZbocKe3CCojYV7o",
+                BasePath = "https://to-do-list-wpf.firebaseio.com/"
+            };
+
+            public bool Connect()
+            {
+                try
+                {
+                    client = new FireSharp.FirebaseClient(config);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            public User GetUser(string username)
+            {
+                FirebaseResponse response = client.Get("ToDoList/Users/" + username);
+                return response.ResultAs<User>();
+            }
+
+            public User GetAllUsers()
+            {
+
+                FirebaseResponse response = client.Get("ToDoList/Users");
+                return response.ResultAs<User>();
+            }
+            public async Task<User> AddUser(User u)
+            {
+                SetResponse response = await client.SetTaskAsync("ToDoList/Users/" + u.Username, u);
+                return response.ResultAs<User>();
+
+
+            }
+
+        }
+    }
+
+}
---
+++ to-do-list-wpf/Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Morning_Bell;
+using System.Threading.Tasks;
+
+namespace to_do_list_wpf.Model
+{
+    public class User
+    {
+        public User() { }
+        public User(string username, int password, string email, Morning_Bell.Task taskID)
+        {
+            this.Username = username;
+            this.Email = email;
+            this.Password = password;
+            this.TasksId = taskID;
+
+
+        }
+        public string Username { get; set; }
+        public int Password { get; set; }
+        public string Email { get; set; }
+        public Morning_Bell.Task TasksId { get; set; }
+    }
+    /* public class Users
+     {
+         public Array User { get; set; }
+     }*/
+}
